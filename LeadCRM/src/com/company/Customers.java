605e68e6179764c7c56f7552db81efe3b6b4e9b4
Khai Ngo <dothat0@gmail.com>
2020-09-03 00:24:19 +0700

    reorganize file

---
+++ LeadCRM/src/com/company/Customers.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.util.Date;
+
+public class Customers {
+    private String name;
+    private String phoneNumber;
+    private boolean gender;
+    private Date birthDate;
+    private String email;
+    private String address;
+    private Leads leadId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ LeadCRM/src/com/company/Manage.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Manage {
+    private Manage manage = new Manage();
+    private Manage() {}
+
+}
